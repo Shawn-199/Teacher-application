aebd98943fd39946c809726cb53cbc94a0f4e9b2
Shawn <grand.english.courses@gmail.com>
2025-10-08 15:26:19 +0500

    fix: balance braces in transporter.verify() block

--- server.js
+++ server.js
@@ -177,8 +177,8 @@ const transporter = nodemailer.createTransport({
   family: 4            // force IPv4 (avoids some IPv6 timeouts on hosts)
 });
 
-// Test connection on startup with verbose logging
-transporter.verify(function(error, success) {
+// Test connection on startup
+transporter.verify((error, success) => {
   if (error) {
     console.error('SMTP connection failed:', error && (error.stack || error));
   } else {
